2768c1a8bcef394d3c50904364aabc77e68152b0
bhushanmandava <162395532+bhushanmandava@users.noreply.github.com>
2025-01-21 17:28:13 -0600

    push

--- WEB-DEVLOPMENT/YELPCAMP/models/campground.js
+++ WEB-DEVLOPMENT/YELPCAMP/models/campground.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
-const { title } = require('process');
+const Review = require('./reviews')
+
 const Schema = mongoose.Schema;
+
 const CampgroundSchema = new Schema({
     title: String,
-    price: String,
+    image: String,
+    price: Number,
     description: String,
-    location: String
-})
-module.exports = mongoose.model('Campground', CampgroundSchema)
+    location: String,
+    reviews: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Review'
+    }]
+});
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
